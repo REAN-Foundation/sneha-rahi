c695a1696c723d09f50be83a2998d82d706ce273
Kiran Kharade <82871552+kiran-rean@users.noreply.github.com>
2023-08-25 12:56:14 +0530

    Merge issues (#55)
    * Issues fixes - August 23 (#44)
    
    * Additional check for empty and null other info in profile (#45)
    
    * Issues fixes - August 23
    
    * Added check for profile other information
    
    * Added missing phone sanitization (#48)
    
    * Adding temp files to static/temp folder
    
    * Adding temp files to static/temp folder (#50)
    
    * Upload file issues 2 (#52)
    
    * Adding temp files to static/temp folder
    
    * Add additional route to upload files
    
    * Fixed minor issues with language and layout.

--- src/routes/api/server/file-resources/upload/+server.ts
+++ src/routes/api/server/file-resources/upload/+server.ts
@@ -1,7 +1,8 @@
-import { uploadBinary } from '../../../services/file.resource'
+import { upload } from '../../../services/file.resource'
 import type { RequestEvent, RequestHandler } from './$types';
 import * as fs from 'fs';
 import * as path from 'path';
+// import { Readable } from 'stream';
 
 //////////////////////////////////////////////////////////////
 
@@ -20,24 +21,38 @@ export const POST = (async (event: RequestEvent) => {
 		console.log(filename);
 
 		const fileBuffer = data_['image'];
-		const filePath = path.join(process.cwd(), `static/temp/${filename}`);
-		//console.log(fileBuffer);
+
+		//////////////////////////////////////////////////////////////
+		let filePath = path.join(process.cwd(), `/temp/`);
+		if (!fs.existsSync(filePath)) {
+			fs.mkdirSync(filePath, { recursive: true });
+		}
+		filePath = path.join(filePath, filename);
 		console.log(filePath);
 		fs.writeFileSync(filePath, fileBuffer, 'base64');
-
 		if (fs.existsSync(filePath)) {
 			console.log(Date.now().toString());
 			console.log(`Copied file ${filename} to server stats/temp.`);
 		}
+		//const buffer = fs.readFileSync(filePath);
+		//////////////////////////////////////////////////////////////
 
 		const sessionId = event.locals.sessionUser.sessionId;
-		const buffer = fs.readFileSync(filePath);
 
 		console.log('Uploading file resource ...');
-		//const location = path.join(process.cwd(), filePath);
-		const response = await uploadBinary(
+
+		//const buffer = Buffer.from(fileBuffer, 'base64');
+		//console.log(buffer);
+		//const response = await uploadBinary(
+		// 	sessionId,
+		// 	buffer,
+		// 	filename,
+		// 	true
+		//);
+
+		const response = await upload(
 			sessionId,
-			buffer,
+			filePath,
 			filename,
 			true
 		);
@@ -48,8 +63,8 @@ export const POST = (async (event: RequestEvent) => {
 		return new Response(JSON.stringify(response));
 
 	} catch (err) {
-		//console.error(`Error uploading file: ${err.message}`);
-		console.error(`Error uploading file: ${JSON.stringify(err.stack.split('\n'), null, 2)}`);
+		console.error(`Error uploading file : ${err.message}`);
+		console.error(`Error stack : ${JSON.stringify(err.stack.split('\n'), null, 2)}`);
 		return new Response(err.message);
 	}
 }) satisfies RequestHandler;
