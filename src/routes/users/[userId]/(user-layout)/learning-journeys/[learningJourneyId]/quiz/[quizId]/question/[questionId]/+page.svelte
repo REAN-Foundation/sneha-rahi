0e6cddbb823a1e91ccaf115a23d145dbbdf0b2a4
Kiran Kharade <kiran.kharade@reanfoundation.org>
2023-11-25 22:18:49 +0530

    Added nextbutton conditionally for quiz

--- src/routes/users/[userId]/(user-layout)/learning-journeys/[learningJourneyId]/quiz/[quizId]/question/[questionId]/+page.svelte
+++ src/routes/users/[userId]/(user-layout)/learning-journeys/[learningJourneyId]/quiz/[quizId]/question/[questionId]/+page.svelte
@@ -3,6 +3,8 @@
 	import QuizMultiChoice from './quiz.multi.choice.svelte';
 	import { page } from '$app/stores';
 	import type { PageServerData } from './$types';
+	import { browser } from "$app/environment";
+
 	// import { onMount } from 'svelte';
 
 	export let data: PageServerData;
@@ -79,59 +81,77 @@
 		//goto(redirectPath);
 	};
 
+	const goForwardInHistory = () => {
+		if (browser) {
+			const hasForwardPages = window.history.length > 1;
+			if (hasForwardPages) {
+				history.forward();
+			}
+			else {
+				window.location.href = `/users/${userId}/learning-journeys/${learningJourneyId}`;
+			}
+		}
+	};
+
 </script>
-
-<!-- <div class="card card-compact w-[375px] h-[701px]  bg-base-100 card-bordered border-slate-200 rounded-none rounded-t-[44px] shadow-sm"> -->
-	<div class="card-body ">
-		<!-- <button class="h-[5px] w-[73px] bg-[#e3e3e3] flex ml-36 mt-2 rounded" /> -->
-		<h3 class=" text-[#5b7aa3] flex tracking-widest justify-center font-bold text-base ">
-			QUIZ
-		</h3>
-		<h3 class="text-[#5b7aa3] justify-center">{data.quiz.Title}</h3>
-		<div class="flex mb-2 ">
-			<h2 class=" text-center text-base font-bold ">
-				{data.currentQuestion.Sequence} of {data.quiz.TotalNumberOfQuestions}
-			</h2>
-		</div>
-		<div class=" bg-[#ffdbb2] rounded-full h-[10px]">
-			<div
-				class="bg-[#fcaf58] rounded-full h-[10px]"
-				style="width:{(data.currentQuestion.Sequence / data.quiz.TotalNumberOfQuestions) * 100}%"
-			/>
-		</div>
-		<!-- {#if !(questionPointer > answers.length - 1)} -->
-		<div class="h-[400px] overflow-auto scrollbar-medium">
-			<p class=" text-left mt-3 mb-3 font-bold text-lg">
-				{data.currentQuestion.Title}
-			</p>
-			{#if isMultichoice}
-				<QuizMultiChoice
-					options={options}
-					answerSubmitted={answerSubmitted}
-					alreadyAnswered = {alreadyAnswered}
-					on:answerSelected={onAnswerSelected}>
-				</QuizMultiChoice>
-			{:else}
-				<QuizSingleChoice
-					options={options}
-					correctSequence={correctSequence}
-					answerSubmitted={answerSubmitted}
-					alreadyAnswered = {alreadyAnswered}
-					on:answerSelected={onAnswerSelected}>
-				</QuizSingleChoice>
-			{/if}
-		</div>
-		<div class="flex justify-center">
-			<button
-				on:click|once={handleSubmit}
-				id="submit"
-				name="submit"
-				disabled={!answerSubmitted || alreadyAnswered}
-				class=" bg-[#5b7aa3] disabled:bg-[#7d7d7d] h-[52px] w-[340px] max-[425px]:w-full mt-4 mb-4 text-[#fff] justify-center rounded-lg"
-			>
-				SUBMIT
-			</button>
-		</div>
+<div class="card-body ">
+	<!-- <button class="h-[5px] w-[73px] bg-[#e3e3e3] flex ml-36 mt-2 rounded" /> -->
+	<h3 class=" text-[#5b7aa3] flex tracking-widest justify-center font-bold text-base ">
+		QUIZ
+	</h3>
+	<h3 class="text-[#5b7aa3] justify-center">{data.quiz.Title}</h3>
+	<div class="flex mb-2 ">
+		<h2 class=" text-center text-base font-bold ">
+			{data.currentQuestion.Sequence} of {data.quiz.TotalNumberOfQuestions}
+		</h2>
 	</div>
-<!-- </div> -->
-
+	<div class=" bg-[#ffdbb2] rounded-full h-[10px]">
+		<div
+			class="bg-[#fcaf58] rounded-full h-[10px]"
+			style="width:{(data.currentQuestion.Sequence / data.quiz.TotalNumberOfQuestions) * 100}%"
+		/>
+	</div>
+	<!-- {#if !(questionPointer > answers.length - 1)} -->
+	<div class="h-[400px] overflow-auto scrollbar-medium">
+		<p class=" text-left mt-3 mb-3 font-bold text-lg">
+			{data.currentQuestion.Title}
+		</p>
+		{#if isMultichoice}
+			<QuizMultiChoice
+				options={options}
+				answerSubmitted={answerSubmitted}
+				alreadyAnswered = {alreadyAnswered}
+				on:answerSelected={onAnswerSelected}>
+			</QuizMultiChoice>
+		{:else}
+			<QuizSingleChoice
+				options={options}
+				correctSequence={correctSequence}
+				answerSubmitted={answerSubmitted}
+				alreadyAnswered = {alreadyAnswered}
+				on:answerSelected={onAnswerSelected}>
+			</QuizSingleChoice>
+		{/if}
+	</div>
+	<div class="flex justify-center">
+		<button
+			on:click|once={handleSubmit}
+			id="submit"
+			name="submit"
+			disabled={!answerSubmitted || alreadyAnswered}
+			class=" bg-[#5b7aa3] disabled:bg-[#7d7d7d] h-[52px] w-[340px] max-[425px]:w-full mt-4 mb-2 text-[#fff] justify-center rounded-lg"
+		>
+			SUBMIT
+		</button>
+	</div>
+	<div class="flex justify-center">
+		{#if alreadyAnswered}
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<div class="link" on:click={goForwardInHistory}>
+				<button class=" bg-[#5b7aa3] h-[52px] w-[340px] max-[425px]:w-full mt-1 mb-4 text-[#fff] justify-center rounded-lg">
+				NEXT
+				</button>
+			</div>
+		{/if}
+	</div>
+</div>
